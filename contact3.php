39a9959aee8b411be78c471530c6c1e0bffe7a04
Andy Knudsen <andy@websitetalkingheads.com>
2017-06-16 21:39:58 -0600

    start

--- contact3.php
+++ contact3.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8">
 <TITLE>Talking Heads|Contact us about Custom Website Video, Website Spokesperson, Whiteboard Video or Animation.</TITLE>
 <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
 <meta name="keywords" content="Whiteboard video, Explainer Video, Animation, Sales video, online spokesperson, video spokesperson, website talking heads, website actor, website video, transparent flash, virtual spokesperson, spokesperson, video presenter, website presenter, website spokesperson, video salesperson">
@@ -19,38 +19,30 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<<<<<<< HEAD
-<link href="/css/style.css?v=<?php echo rand(1,100); ?>" rel="stylesheet" type="text/css" />
-<link href="css/products.css" rel="stylesheet" type="text/css">
-<link href="/css/fluid.css" rel="stylesheet" type="text/css" />
-=======
-<link href="https://websitetalkingheads.com/css/fluid.css" rel="stylesheet" type="text/css" />
-<link href="https://websitetalkingheads.com/css/style.css?v=<?php echo rand(1,100); ?>" rel="stylesheet" type="text/css" />
-<link href="css/products.css" rel="stylesheet" type="text/css">
->>>>>>> start
+<link href="css/style.css?v=<?php echo rand(1,100); ?>" rel="stylesheet" type="text/css" />
+<link href="css/fluid.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <?php include ('includes/header.php'); ?>
-<section class="container">   
-	<div class="row">  
-    	<div class="contactus col-md-6 no-shadow">
-            <h1 class="no-shadow">Contact Information</h1>
-            <h2 class="no-shadow">:: Mailing address ::</h2>
-            <h3 class="no-shadow"> Website Talking Heads<sup>&reg;</sup><br>
-                245 W. 9000 S. <br>
-                Sandy, UT 84070 </h3>
-            <h2 class="no-shadow">:: Phone :: </h2>
-            <h3 class="no-shadow"><a href="tel://801-748-2281" title="Give us a call.">1-801-748-2281</a></h3>
-            <h2 class="no-shadow">:: Inquiries and Questions :: </h2>
-            <h3 class="no-shadow"><a href="mailto:Sales@websitetalkingheads.com" title="Drop us a line.">Sales@WebsiteTalkingHeads.com</a></h3>
-        </div>
-        <div class="col-md-6">
-        	<?php include ('forms/contactform.php'); ?>
-        </div>
-     </div>       
+<section class="container">
+<div class="row center-block">
+<div class="contactus col-md-6">
+  <h1>Contact Information</h1>
+  <h2>:: Mailing address ::</h2>
+  <h3> Website Talking Heads <br>
+    245 W. 9000 S. <br>
+    Sandy, UT 84070 </h3>
+  <h2>:: Phone :: </h2>
+  <h3><a href="tel://801-748-2281" title="Give us a call.">1-801-748-2281</a></h3>
+  <h2>:: Inquiries and Questions :: </h2>
+  <h3><a href="mailto:Sales@websitetalkingheads.com" title="Drop us a line.">Sales@WebsiteTalkingHeads.com</a></h3>
+</div>
+<div class="col-md-6">
+<?php include ('forms/vtiger-contact-form.php'); ?>
+</div>
+</div>
 </section>
 <?php include ('https://www.websitetalkingheads.com/includes/CallForQuote.php'); ?>
 <?php include ('includes/footer.php'); ?>
-<?php include ('includes/chatform.php'); ?>
 </body>
 </html>
