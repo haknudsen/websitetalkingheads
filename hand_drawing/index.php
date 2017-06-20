c7a5fca7a2bb73f2f13472ced8d888a0ba6b4ab3
Andy Knudsen <andy@websitetalkingheads.com>
2017-06-20 15:35:39 -0600

    Updated landing pages

---
+++ hand_drawing/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Whiteboard Video, Animation, Explainer Video, Video Scribe, Hand Drawing Video, Chalkboard Video, Online Spokesperson, Video Spokesperson, Website Talking Heads, Website Actor, Website Video, Virtual Spokesperson, Spokesperson, Video Presenter, Website Presenter, Website Spokesperson">
+    <meta name="description" content="Talking Heads can create a Whiteboard Video, Animation, Explainer Video, Video Scribe, Hand Drawing Video, Chalkboard Video.  Here you watch examples of these kinds of videos.  Whether you can call it a Sketch Video, Chalkboard Video, Hand Drawing Video, Whiteboard Animation, or Animated Explainer Video Talking Heads&reg; will make the best video for you.  Contact us for more information.">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="WebsiteTalkingHeads.com">
+    <title>Hand Drawing Animation|Talking Heads®</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link href="css/fluid.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" type="text/css" href="https://www.websitetalkingheads.com/lightbox/js/lightbox/themes/default/jquery.lightbox.css"/>
+    <?php include ('../includes/googleanalytics.php'); ?>
+</head>
+
+<body>
+    <section class="wrap">
+        <div class="header">
+            <div class="logo"><a href="index.php"></a>
+            </div>
+            <div class="header-right">
+                <div class="nav"><a href="index.php">Home</a> | <a href="animation.php">Animation</a> | <a href="../contact.php">Contact</a> </div>
+                <div class="phone"><a href="tel://801-748-2281" title="Give us a call.">1-801.748.2281</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="whiteboard-demo">
+        <div class="row center-block">
+            <div class="col-md-7 center-block">
+                <div class="banner-text">Cool Hand Drawing Animation, Whiteboard Sketch Videos, Spokesperson too!</div>
+                <div class="embed-responsive embed-responsive-16by9  banner-video">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/NiCdoZ_7kcA?rel=0&autohide=2&showinfo=0" allowfullscreen> </iframe>
+                </div>
+            </div>
+            <div class="get-a-quote col-md-5">
+                <?php include("includes/form.php"); ?>
+            </div>
+            <div class="c"></div>
+        </div>
+    </section>
+    <section class="container">
+        <div class="banner-bottom-text">Hand Drawing Animation, Whiteboard Animation Videos, Whiteboard Sketch, Video Scribe and Cartoon Doodle Videos.</div>
+        <div class="content-text">At Talking Heads we don't just make hand drawn animation. We have spent years studying the art of persuasion and communication. Your story must be told well. So we collaborate to customize and create a video that boost leads and increases sales in your product or service.</div>
+        <div class="examples clearfix">
+            <?php include ('../whiteboard/includes/examples-whiteboard-full.php'); ?>
+        </div>
+        <div class="video-bottom"></div>
+        <div class="call-now center-block">
+            <h2>CALL NOW to Get Results!</h2>
+            <h3><a href="tel://801-748-2281" title="Give us a call.">801-748-2281</a></h3>
+        </div>
+        <div class="ten"></div>
+        </div>
+    </section>
+    <section class="container pad-10 clearfix">
+        <div class="no-1">
+            <h1 class="text-center">Hand Drawing Animation</h1>
+            <img src="../images/video-production-seal.png" alt="#1 Video Production" width="340" title="Best in Search #1 Video Production"/>
+            <h3>Keep your visitor's attention and boost sales.</h3>
+            <p>At Talking Heads we don't just make Hand Drawing Animations. We have spent years studying the art of persuasion and communication. Your story must be told well. So we collaborate to customize and create a video that boost leads and increases sales in your product or service.</p>
+            <p>Talking Heads' Sketch videos stimulate your viewers' brains. Mirror neutrons in the brain make the viewer think it's his or her hand drawing, triggering a dopamine rush in the brain and increase the viewers capacity to focus and remember. The brain is now curious and pays more attention wondering what the whiteboard drawing will become. As it tries to guess what the sketch will become, it focuses more and rewards itself. This happens over and over. Whiteboard sketch is like a time machine with each new drawing practically hypnotizing your viewer. Because of this hyper focus, the viewer also remembers more of your whiteboard animation that regular videos. here, there is the entire bottom</p>
+        </div>
+    </section>
+    <section class="container pad-10">
+        <?php include("includes/awards.php"); ?>
+    </section>
+    <section class="footer-wrap">
+        <div class="footer">© 2016, <a href="https://websitetalkingheads.com" target="_blank">WebsiteTalkingHeads.com</a>. All Rights Reserved.</div>
+        </div>
+    </section>
+    <?php include ('../includes/googlescripts.php'); ?>
+    <script src="https://websitetalkingheads.com/lightbox/js/jquery.min.js"></script>
+    <script src="https://websitetalkingheads.com/lightbox/js/lightbox/jquery.lightbox.min.js"></script>
+    <script src="includes/js.js"></script>
+
+    <noscript>
+        <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1058129782/?value=0&amp;label=-jOyCO6ZkAIQ9o7H-AM&amp;guid=ON&amp;script=0"/>
+        </div>
+    </noscript>
+</body>
+
+</html>
