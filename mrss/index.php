03d73f03c4aec03528d5ee1b4fb3a047aa1c8873
Andy Knudsen <andy@websitetalkingheads.com>
2017-06-21 21:59:11 -0600

    updated MRSS

--- mrss/index.php
+++ mrss/index.php
@@ -1,57 +1,51 @@
-<!doctype html>
-<html><!-- InstanceBegin template="/Templates/NewPhp.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- InstanceBeginEditable name="doctitle" -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Website Talking Heads - Add an Online Video Spokesperson to Your Website, Virtual Spokesperson, Website Video Spokesperson, Web Spokesperson, Website Actor</title>
-<link href="../css/feed.css" rel="stylesheet" type="text/css" />
-<!-- InstanceEndEditable -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
 <meta name="keywords" content="online spokesperson, video spokesperson, website talking heads, website actor, website video, transparent flash, virtual spokesperson, spokesperson, video presenter, website presenter, website spokesperson, video salesperson">
-<meta name="description" content="Online video spokesperson.  For only $199, add a virtual spokesperson to your website.  An online presenter can increase traffic conversion rates on your website.  Website video, website actor and objects to create dynamic streaming video and easily add it your existing website.">
-<meta name="robots" CONTENT="index, follow"> <!-- (Robot commands: All, None, Index, No Index, Follow, No Follow) -->
-<meta name="revisit-after" CONTENT="30 days">
+<meta name="description" content="Online video spokesperson.  For only $199, add a virtual spokesperson to your website.  An online presenter can increase traffic conversion rates on your website.  Integrate flash video, website video, website actor and objects to create dynamic streaming video and easily add it your existing website.">
+<META NAME="robots" CONTENT="index, follow"> <!-- (Robot commands: All, None, Index, No Index, Follow, No Follow) -->
+<META NAME="revisit-after" CONTENT="30 days">
+<META NAME="distribution" CONTENT="global">
+<META NAME="rating" CONTENT="general">
+<META NAME="Content-Language" CONTENT="english">
 <meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8=" />
 <meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-<link href="https://websitetalkingheads.com/css/style.css?v=<?php echo rand(1,100); ?>" rel="stylesheet" type="text/css" />
-<link href="https://www.websitetalkingheads.com/css/fluid.css" rel="stylesheet" type="text/css" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="../css/style.css" rel="stylesheet" type="text/css" />
+<link href="../css/feed.css" rel="stylesheet" type="text/css" />
+<link href="../css/fluid.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-<!-- InstanceBeginEditable name="MainContent" -->
 <div id="container">
-<div align="center">
+  <div align="center">
     <?php include ('../includes/header.php'); ?>
     <h1>Our mRSS Feeds</h1><img src="../feed-icon-28x28.png" width="28" height="28" alt="RSS Feed"/>
-</div>
-<ul class="feed-list">
-  <li><a href="https://www.youtube.com/rss/user/wthvideoproduction/feed.rss">WTH Video Production YouTube Feed</a></li>
-  <li><a href="https://www.youtube.com/rss/user/webtalkingheads/feed.rss">Web Talking Heads YouTube Feed</a></li>
-  <li><a href="actors.rss">Our Actors</a></li>
-  <li><a href="featuredactors.rss">Featured Actors</a></li>
-  <li><a href="sitevideos.rss">Site Videos</a></li>
-  <li><a href="animation.rss">Animation Examples</a></li>
-  <li><a href="custom.rss">Custom Video Examples</a></li>
-  <li><a href="powerpunch.rss">Power Punch Video Examples</a></li>
-  <li><a href="virtual.rss">Virtual Set Examples</a></li>
-  <li><a href="whiteboard.rss">Whiteboard Video Examples</a></li>
-  <li><a href="typography.rss">Typography Examples</a></li>
-  <li><a href="outro.rss">Ouro Examples</a></li>
-  <li><a href="wthvideo.rss">All Videos</a></li>
-</ul>
-<br/>
-
-<h2><a href="rsscreate.php" target="new">Create the FEEDs</a></h2>
-
-<?php include ('../includes/footer.php'); ?>   
+  </div>
+  <ul class="feed-list">
+    <li><a href="https://www.youtube.com/feeds/videos.xml?channel_id=UCKV3naM0IK4nbPDoUR1Yu7A">WTH Video Production YouTube Feed</a></li>
+    <li><a href="https://www.youtube.com/feeds/videos.xml?channel_id=UCOedyOlBOjRaQWWFfNnEIlw">Web Talking Heads YouTube Feed</a></li>
+    <li><a href="actors.rss">Our Actors</a></li>
+    <li><a href="featuredactors.rss">Featured Actors</a></li>
+    <li><a href="sitevideos.rss">Site Videos</a></li>
+    <li><a href="animation.rss">Animation Examples</a></li>
+    <li><a href="custom.rss">Custom Video Examples</a></li>
+    <li><a href="powerpunch.rss">Power Punch Video Examples</a></li>
+    <li><a href="virtual.rss">Virtual Set Examples</a></li>
+    <li><a href="whiteboard.rss">Whiteboard Video Examples</a></li>
+    <li><a href="typography.rss">Typography Examples</a></li>
+    <li><a href="outro.rss">Ouro Examples</a></li>
+    <li><a href="wthvideo.rss">All Videos</a></li>
+  </ul>
+  <br/>
+  
+  <h2><a href="rsscreate.php" target="new">Create the FEEDs</a></h2>
+  
+  <?php include ('../includes/footer.php'); ?>   
 </div>
 
 <?php include ('../includes/chatform.php'); ?>  
-<!-- InstanceEndEditable -->  
 </body>
-<!-- InstanceEnd --></html>
+</html>
