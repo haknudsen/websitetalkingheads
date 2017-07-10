4538963ffdd180d62e0013e504de9ec819fc0cc6
haknudsen <paris_giovanni@yahoo.com>
2017-07-10 13:00:32 -0600

    Updating show demo app

---
+++ demopage/demo2017.php
@@ -0,0 +1,14 @@
+<section class="SpokespersonVideos">
+  <form name="demo" method="post" action="https://www.websitetalkingheads.com/demopage/showdemo2017.php" target="_blank">
+    <h2>Try a video spokesperson on your site now!</h2>
+    <div class="search">
+      <div class="row">
+        <div class="col-sm-2 web-url center-block">Website URL</div>
+        <input class="col-sm-8 center-block" name="website" type="text" id="website" onblur="if(this.value=='') {this.value='https://www.websitetalkingheads.com/contact3.php';}" onfocus="if(this.value=='https://www.websitetalkingheads.com/contact3.php') {this.value='';}" value="https://www.websitetalkingheads.com/contact3.php" />
+        <div class="col-sm-2 center-block">
+          <input type="submit" class="view center-block"  title="Click to see a Video Spokesperson on Your Site" tabindex="16" name="submit" id="submit" value="Submit" />
+        </div>
+      </div>
+    </div>
+  </form>
+</section>
---
+++ demopage/showdemo2017.php
@@ -0,0 +1,55 @@
+<?php
+$referersite = strtolower( $_SERVER[ 'HTTP_REFERER' ] );
+$url = $_REQUEST[ 'website' ];
+if ( preg_match( '#^https?://#i', $url ) === 1 ) {
+    $s = true;
+    $website = $url;
+} else {
+    $website = "http://" . $url;
+    $s = false;
+}
+if ( isset( $_SERVER[ 'HTTP_X_FORWARDED_FOR' ] ) && $_SERVER[ 'HTTP_X_FORWARTDED_FOR' ] != '' ) {
+    $ip_address = $_SERVER[ 'HTTP_X_FORWARDED_FOR' ];
+} else {
+    $ip_address = $_SERVER[ 'REMOTE_ADDR' ];
+}
+//	$to = 'andy@websitetalkingheads.com, sales@websitetalkingheads.com';
+//	$subject = "Email Demo Link -  ".$referersite;
+//	$headers = "MIME-Version: 1.0" . "\r\n";
+//	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+//	$headers .= "From: forms@websitetalkingheads.com\r\n";
+//	$email_message = "Email Demo Link -  ".$referersite."<br>";
+//	$email_message .= "Website Demoed:" .$website. "<br>";
+//	$email_message .= "Viewed From: ".$ip_address."<br>";
+//	mail($to,$subject,$email_message,$headers);
+if ( $s ) {
+    $page = '
+<!doctype html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Website Talking Heads</title>
+<style type="text/css">
+body {
+	padding: 0;
+	margin: 0;
+}
+</style>
+</head>
+<body>
+<script src="wthvideo/wthvideo.js"></script>
+<iframe src="';
+    $page .= $website;
+    $page .= '" frameborder="0" width="100%" height="2000" scrolling="No" id="theFrame" name="theFrame"></iframe>
+    <script>
+        window.onload = function () {
+            wthplayer();
+        }
+    </script>
+</body>
+</html>';
+
+}else{
+    $page=file_get_contents($website);
+}
+print_r( $page );
+?>
