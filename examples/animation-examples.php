dbc33ffc58b9e1f0510a1b6202f0121fa8cbe834
Andy Knudsen <andy@websitetalkingheads.com>
2017-07-18 22:12:16 -0600

    made example order by item_order
    added content to bottom.

--- examples/animation-examples.php
+++ examples/animation-examples.php
@@ -1,7 +1,7 @@
 <?php
 require("connect.php");
 $table = "animation"; 
-$sql = "SELECT * FROM " . $table . " ORDER BY name";
+$sql = "SELECT * FROM " . $table . " ORDER BY list_order";
 $result = $conn->query( $sql );
 
 if ( $result->num_rows > 0 ) {
@@ -15,7 +15,7 @@ while($row = $result->fetch_assoc()) {
     $video = $row[ "name" ];
     echo '<div class="col-sm-4">';
     echo PHP_EOL;
-    echo '<a title="' . $video . '" href="https://www.youtube.com/watch?v=' . $target . '&rel=0&autoplay=1&hd=1" class="lightbox"><img src="https://img.youtube.com/vi/' . $target . '/mqdefault.jpg" class="img img-responsive box" id="' . $video . '" alt="Animation Example - .$example->name. " ></a>';
+    echo '<a title="' . $video . '" href="https://www.youtube.com/watch?v=' . $target . '&rel=0&autoplay=1&hd=1" class="lightbox"><img src="https://img.youtube.com/vi/' . $target . '/maxresdefault.jpg" class="img img-responsive box" id="' . $video . '" alt="Animation Example - '.$example->name.' " ></a>';
     echo PHP_EOL;
     echo '<h3 class="example-text"><a title="' . $video . '" href="https://www.youtube.com/watch?v=' . $target . '&rel=0&autoplay=1&hd=1" class="lightbox">' .$video . '</a></h3>'; 
     echo PHP_EOL;
---
+++ examples/animation-media.php
@@ -0,0 +1,72 @@
+<?php
+$mediaHeading = array( "Web Video", "Animation", "Specialized Video" );
+$mediaBody = array( "<p>Web video {marketing|internet marketing|marketing and advertising|promoting|promotional} {or|and|and also|or even|or simply} {digital|web|information} {marketing|internet marketing|marketing and advertising|promoting|promotional} {is the|is a| be the|may be the|will be the} {growing|rising|increasing|escalating|raising} {trend|phenomena|craze|movement|fad} {among|with|involving|amongst} {companies|agencies|businesses|organizations|organisations} {who want to|who wish to|who would like to} {stay|continue to be|remain} {competitive|aggressive|ambitious} {and} {relevant|applicable|useful|focused} {in the|inside|with the|within the|inside the} {digital|web|information} {age|grow old|age group|period|their age}. {To be able to|So as to|For you to|As a way to} {demand|require|recieve} {consumer|customer|buyer|shopper|end user} {attention|particular attention|curiosity|notice|awareness}, {you need to|you might want to|you have to|you must|you'll want to} {deliver|offer|give|send out|provide} {your|one's|one's own|a|a person's} {message|principles|note|sales message|meaning} {through the|in the|with the|over the|on the} {most efficient|most effective|best|handiest|well organized} {and} {reliable|well-performing|trusted|dependable|efficient} {source|origin|base|form|resource} {there is|there does exist|there's|you can find|there exists}: {social media|social networking|social media marketing|web 2.0}. {Gone|Departed|Vanished|Eliminated} {are the|are definitely the|will be the} {days|times} {when|as soon as|any time|when ever|the moment} {simple|very simple|basic|uncomplicated|effortless} {advertisements|promotions|commercials|advertisings|adverts} {or|and|and also|or even|or simply} {articles|article content|articles and reviews|reports|articles or blog posts} {are effective|work|are impressive} {enough|more than enough|adequate|sufficiently|sufficient} {in|with|within|around|inside} {capturing|harvesting|collecting|taking|getting} {the|that|this|your|a} {audience|crowd|target market|customers|target audience} {you want|you would like|you need|you prefer|you are looking for}. {Research|Explore|Exploration|Homework|Study} {shows that|demonstrates|implies that|demonstrates that|ensures that} {the most effective|the most beneficial|the best quality|the very best|the most efficient} {means of|methods for|methods of|method of|technique of} {delivering|providing|giving you|serving|giving} {your|one's|one's own|a|a person's} {message|principles|note|sales message|meaning} {to|to help|to help you|so that you can|to be able to} {today's|current|the present|modern day|present day} {consumer|customer|buyer|shopper|end user} {public|general public|court|open public|open} {is|is usually|is actually|is normally|can be} {through|as a result of|by way of|because of|with} web video.</p>", "<p>Web {statistics|numbers|reports|report|data} show that marketing {in the|inside|within the|inside the} digital age {has|comes with|has gotten} successfully away from static advertisements {to|on to|into} web videos. Experts have forecasted {that the|that|that this|of the fact that|that your} mobile video {consumption|drinking|use|usage|content} is increasing {by|just by|as a result of|simply by|by way of} 100% with {each|just about every|every different|every single|each and every} passing year {and it|therefore|plus it|and it also|and yes it} will only {continue to|pursue to|carry on and|always|will begin to} grow from {there|truth be told there|at this time there|in that respect there|generally there}. By 2019, {it is|it can be|it's|it happens to be|it truly is} predicted to surpass {any other|some other|other|every other|each and every} media in {the internet|the online market place|the net|the web|the on-line world} by 80%. {If|When|Any time|In the event that|If perhaps} utilized right, video production and animation {can become|may be|develop into|can be|can get} your ticket to being {the next|the following|next|the other|the subsequent} social media brand {phenomenon|trend|method|happening|sensation}.</p>", "<p>{Animated|Computer animated} {drawing|illustrating} {or|and|and also|or even|or simply} doodle videos {focuses|concentrates|works|centers|targets} {more|even more|much more|a lot more|far more} {on the|relating to the|in the|over the|to the} {message|principles|message|sales message|meaning}. {For|With regard to|Meant for|To get|Designed for} non-food {companies|agencies|businesses|organizations|organisations} {where|the place|when|where by|at which} {you cannot|you can not|you may not|it's not possible to|you can't} {use|employ|make use of|benefit from|work with} close-ups {of|with|involving|associated with|from} {delicious|flavorful|delightful|mouth watering|fabulous} {food|foodstuff|nutrition|meals|foods}, {it is better|marketing promotions campaigns|it is far better|it is best|it is advisable} {to be|to remain|being|to become|to get} {direct|immediate} {message|principles|note|sales message} {without|without the need of|free of|not having|with no} {neglecting|ignoring|disregarding|failing|overlooking} {the|that|this|your|a} {entertainment|activities|amusement|pleasure|enjoyment} {factor|issue|component|element|thing} {of|with|involving|associated with|from} video production. {With|Using|By means of|Along with|By using} {effective|successful|powerful|useful|efficient} {and} {engaging|fascinating|getting|practicing} doodle {animation|cartoon|computer animation|toon|movement}, {you|people|everyone|most people|anyone} {would be able to|can|} {secure|protected|safeguarded|acquire|safe} {your|one's|one's own|a|a person's} audience's {attention|particular attention|curiosity|notice|awareness} {while|even though|although|whereas|despite the fact that} {allowing them to|permitting them to|letting them} {clearly|undoubtedly|definitely|plainly|certainly} {register|sign up|apply for|sign-up|make an account} {the information|the internet|the details|the data|the info} {you|people|everyone|most people|anyone} {are trying to|are attempting to|making the effort to|looking to|making the effort} {convey|share|express|indicate|show}. {This|The following|That|This approach|This particular} {style|trend|fashion} {creates|brings about|generates|makes|results in} {an end|a finish|a conclusion} to {product|product or service|item|system|merchandise} {devoid of|without requiring|without the need of|free of|without the need for} distraction {unlike|not like|nothing like|as opposed to|dissimilar to} {what a|such a|thats|a lot of|what} {effects|effects or graphic|graphic|aesthetic} {heavy|serious|large|hefty|significant} web video {will do|is going to do}. {This kind of|Such a|This|Such type of|These types of} {style|trend|fashion} {is perfect for|is designed for|is good for|is perfectly for|is made for} infomercials {and} {companies|agencies|businesses|organizations|organisations} {whose|in whose|as their|whoever} {main goal|definitive goal|absolute goal} {is to|may be to|is always to|should be to|is usually to} {educate|teach|coach|train|show} {and to|and|and also to|and then to|so to} {deliver|offer|give|send out|provide} {information|info|information and facts|facts|tips} {effectively|properly|efficiently|proficiently|comfortably} {in the|inside|with the|within the|inside the} {shortest|quickest|least|least amount|speediest} {amount of time|period of time|period|length of time|long} {possible|achievable|feasible|attainable}.</p>" );
+require( "connect.php" );
+$table = "animation";
+$sql = "SELECT * FROM " . $table . " WHERE list_order > 12";
+$result = $conn->query( $sql );
+
+if ( $result->num_rows > 0 ) {
+    echo '<div class="row">';
+    $count = 2;
+    $x = 0;
+    while ( $row = $result->fetch_assoc() ) {
+        $target = $row[ "target" ];
+        $video = $row[ "name" ];
+        echo '<div class="media">';
+        echo PHP_EOL;
+        if ( $x === 2 ) {
+            echo '<div class="media-right">';
+        } else {
+            echo '<div class="media-left">';
+        }
+        echo PHP_EOL;
+        echo '<a href="#">';
+        echo PHP_EOL;
+        echo '<a title="' . $video . '" href="https://www.youtube.com/watch?v=' . $target . '&rel=0&autoplay=1&hd=1" class="lightbox"><img src="https://img.youtube.com/vi/' . $target . '/maxresdefault.jpg" class="media-object box" id="' . $video . '" alt="Animation Example - '.$example->name.' " ></a>';
+        echo PHP_EOL;
+        echo '</a>';
+        echo PHP_EOL;
+        echo '</div>';
+        echo PHP_EOL;
+        echo '<div class="media-body">';
+        echo PHP_EOL;
+        echo '<h4 class="media-heading text-capitalize">' . $mediaHeading[ $x ] . '</h4>';
+        echo PHP_EOL;
+        $spintax = new Spintax();
+        $spun = $spintax->process( $mediaBody[ $x ] );
+        echo $spun;
+        echo PHP_EOL;
+        echo '</div>';
+        echo PHP_EOL;
+        echo '</div>';
+        echo PHP_EOL;
+        if ( $x == $count ) {
+            break;
+        } else {
+            $x = $x + 1;
+        }
+    }
+} else {
+    echo "0 results";
+}
+echo PHP_EOL;
+echo '</div>';
+echo '<div class="c"></div>';
+
+class Spintax {
+    public
+    function process( $text ) {
+        return preg_replace_callback( '/\{(((?>[^\{\}]+)|(?R))*)\}/x', array(
+            $this,
+            'replace'
+        ), $text );
+    }
+    public
+    function replace( $text ) {
+        $text = $this->process( $text[ 1 ] );
+        $parts = explode( '|', $text );
+        return $parts[ array_rand( $parts ) ];
+    }
+}
+?>
