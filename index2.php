e31e0a84da7b4fb34256b6c1dc38e51ba99d70d2
Andy Knudsen <andy@websitetalkingheads.com>
2017-09-12 20:09:45 -0600

    updated contact form

---
+++ index2.php
@@ -0,0 +1,147 @@
+<!doctype html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <title>Website Talking Heads&reg;|Home of Video Solutions</title>
+    <meta name="keywords" content="HTML5, video, online spokesperson, video spokesperson, website talking heads, website actor, website video, transparent video, virtual spokesperson, spokesperson, video presenter, website presenter, website spokesperson, video salesperson">
+    <meta name="description" content="Website Talking Heads® creates Website Spokesperson, Presentations, Whiteboard and Animation Videos.  We have almost 10 years experience in web video.">
+    <meta name="robots" content="index, follow">
+    <meta name="revisit-after" content="30 days">
+    <meta name="rating" content="general">
+    <meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8="/>
+    <meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <link href="css/home-working.css" rel="stylesheet" type="text/css"/>
+    <link href="css/products.css" rel="stylesheet" type="text/css"/>
+    <link href="css/normalize.css" rel="stylesheet" type="text/css"/>
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="css/fluid.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" type="text/css" href="../lightbox/js/lightbox/themes/default/jquery.lightbox.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="../lightbox/js/lightbox/jquery.lightbox.min_v2.js"></script>
+    <script src="wthvideo/wthvideo.js"></script>
+    <script src="js/evenHeight.js"></script>
+    <script src="js/jquery.plugin.js"></script>
+    <script src="js/jquery.countdown.js"></script>
+</head>
+
+<body>
+    <?php include ('includes/header.php'); ?>
+    <section class="container">
+        <div class="row center-block" style="padding-top:10px">
+            <div class="Spokesperson col-md-4">
+                <div id="wthvideo"></div>
+            </div>
+            <div class="LogoColumn col-md-4">
+                <div class="row">
+                    <h3 class="hidden">Better Business Rating A+</h3>
+                    <div><a id="bbblink" class="ruhzbul" href="https://www.bbb.org/utah/business-reviews/video-production-services/website-talking-heads-in-sandy-ut-13000968#bbbseal" data-toggle="tooltip" title="Website Talking Heads<sup>&reg;</sup>, Video Production Services, Sandy, UT"><img class="img img-responsive videoProduction" style="padding: 0px;max-width:200px; border: none" id="bbblinkimg" src="images/website-talking-heads-13000968.png"  alt="Website Talking Heads, Video Production Services, Sandy, UT" /></a>
+                    </div>
+                </div>
+                <div class="row">
+                    <?php include("includes/seal-random-2.php"); ?>
+                </div>
+                <div class="row">
+                   <?php include("includes/banner-random.php"); ?>
+                    </div>
+            </div>
+            <div class="col-md-4">
+                <?php include ("forms/contact-us.php");?>
+            </div>
+        </div>
+        <div class="line"></div>
+    </section>
+    <?php include ('https://websitetalkingheads.com/demopage/demo-fluid.php'); ?>
+    <section class="SpokespersonVideos container">
+        <div class="row center-block">
+            <h1>Need a Video Spokesperson?</h1>
+            <h2>Website Talking Heads<sup>&reg;</sup> Offers 4 Kinds of Spokesperson Videos</h2>
+            <div class="video-types">
+                <div class="Product CustomPlayer">
+                    <div class="product-top"> <a href="website-spokesperson/index.php"><img class="productimage center-block" src="images/WebsiteVideo.gif"  alt="Custom Video Player"/></a> </div>
+                    <h3 class="ProductName"><a href="website-spokesperson/index.php">#1 Website Spokesperson</a></h3>
+                    <p class="ProductDescription">Add a Spokesperson to your website to welcome visitors and direct them to key areas. Increase conversion rates and boost leads. Uses HTML5 canvas for Transparent effect, but when detects mobile device uses standard MP4. Learn about the <a href="https://www.websitetalkingheads.com/pros_vs_cons/">Pros and Cons of a Website Spokesperson</a>. </p>
+                </div>
+                <div class="Product YouTubeVideo">
+                    <div class="product-top"> <a href="youtube-ready/index.php"><img class="productimage center-block" src="images/YouTubeReady.gif"  alt="YouTube Ready Video"/></a> </div>
+                    <h3 class="ProductName"><a href="youtube-ready/index.php">#2 Add Background and Logo</a></h3>
+                    <p class="ProductDescription"> You can choose a powerful YouTube and Facebook compatible sales video. Choose an actor, send your script and logo. We put it all together and provide you a Full HD 1920X1080 video in MP4 format. Upload it to YouTube, Facebook, Twitter, Instagram, etc...</p>
+                </div>
+                <div class="Product GreenScreen">
+                    <div class="product-top"> <a href="green-screen-video/index.php"><img class="productimage center-block" src="images/GreenScreenVideo.gif" alt="Green Screen Video"/></a> </div>
+                    <h3 class="ProductName"><a href="green-screen-video/index.php">#3 Green Screen File</a></h3>
+                    <p class="ProductDescription">Attention Marketing and Ad Agencies, professional editors and compositiors:</p>
+                    <p class="ProductDescription">Save time and <strong>HUNDREDS</strong> of dollars. We will do the filming and provide you with a hi-res file to use in your Internet video project.</p>
+                </div>
+                <div class="c"></div>
+            </div>
+        </div>
+        <div class="row center-block">
+            <h1>Custom Videos</h1>
+            <h2>Three Types of Custom Videos</h2>
+            <div class="video-types">
+                <div class="Product VideoPresentation">
+                    <a class="lightbox box" title="Video Presentation - Quote Zoom" href="https://www.youtube.com/watch?v=ozwgqkGubFo?rel=0&autoplay=1&hd=1" target="_blank">
+                        <div class="thumb-wrapper">
+                            <div class="overlay-med"></div>
+                            <img class="productimage center-block" src="https://img.youtube.com/vi/ozwgqkGubFo/mqdefault.jpg" id="QuoteZoom" title="Video Presentation - Quote Zoom" alt="Video Presentation - Quote Zoom"> </div>
+                    </a>
+                    <h3 class="ProductName"><a href="videopresentations/custom-presentations.php"> Video Presentation</a></h3>
+                    <p class="ProductDescription"> With our Custom Video Presentations you get a Video Spokesperson, Professional Editing, Motion Graphics, and Lots of Images and Effects. Great for your Corporate or Sales Video, a Product Demonstration or Demonstration of your Website or Software. We put it all together and provide you a Full HD 1920X1080 video.</p>
+                </div>
+                <div class="Product Whiteboard">
+                    <div class="product-top">
+                        <a class="lightbox" title="Custom Whitebord Explainer Video" href="https://www.youtube.com/watch?v=ZyDepihbT7Y?rel=0&autoplay=1&hd=1" target="_blank">
+                            <div class="thumb-wrapper">
+                                <div class="overlay-med"></div>
+                                <img class="productimage center-block" src="https://img.youtube.com/vi/ZyDepihbT7Y/mqdefault.jpg" id="WhitebordExplainer" title="Whiteboard Explainer Video Example" alt="Custom Whitebord Explainer Video">
+                            </div>
+                        </a>
+                    </div>
+                    <h3 class="ProductName"><a href="whiteboard/">Whiteboard Explainer Video</a></h3>
+                    <p class="ProductDescription">We use fast-paced, brain stimulated imagery and content to ignite your customers build their interest in your solution and ignite their buying habits. Includes Collaborative Scripting, Creative Storyboard and Custom Drawings. We create high impact, exciting VIDEOS that explain your product, service, software or business.</p>
+                </div>
+                <div class="Product Animation">
+                    <div class="product-top">
+                        <a class="lightbox" title="Animation Example Video" href="https://www.youtube.com/watch?v=w6mggrEuUI0?rel=0&autoplay=1&hd=1" target="_blank">
+                            <div class="thumb-wrapper">
+                                <div class="overlay-med"></div>
+                                <img class="productimage center-block" src="https://img.youtube.com/vi/w6mggrEuUI0/mqdefault.jpg" id="AnimationVideo" title="Animation Video" alt="Animation Example Video">
+                            </div>
+                        </a>
+                    </div>
+                    <h3 class="ProductName"><a href="https://www.websitetalkingheads.com/videopresentations/animation.php">Animation Videos</a></h3>
+                    <p class="ProductDescription">Talking Heads&reg; can explain your product, service or procedure in an Animated Explainer Video! Our Animated Explainer Videos powerfully explain things step-by-step, and it keep your viewers' attention! We create custom video content tailored for your business and marketing objectives. </p>
+                </div>
+                <div class="c"></div>
+            </div>
+        </div>
+        <div class="c"></div>
+    </section>
+    
+    <?php include ('includes/footer.php'); ?>
+    <?php include ('includes/chatform.php'); ?>
+    <script>
+        var script = document.createElement( 'script' );
+        var prefix = document.location.protocol;
+        script.async = true;
+        script.type = 'text/javascript';
+        var target = prefix + '//app.clixtell.com/scripts/latest.js';
+        script.src = target;
+        var elem = document.head;
+        elem.appendChild( script );
+        $( window ).load( function () {
+            $( '.lightbox' ).lightbox();
+            $( '[data-toggle="tooltip"]' ).tooltip();
+            $( '.video-types' ).find( '.Product' ).equalizeHeight();
+            $( '.history-spokesperson' ).find( '.h-s' ).equalizeHeight();
+            $( '#about' ).find( '.introduction' ).equalizeHeight();
+        } );
+    </script>
+</body>
+
+<
+/html>
