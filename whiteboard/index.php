6ed5139858557ee312608e98235e4d8ae77b4a13
Andy Knudsen <andy@websitetalkingheads.com>
2017-10-06 12:14:23 -0600

    working

--- whiteboard/index.php
+++ whiteboard/index.php
@@ -40,75 +40,20 @@
         </div>
     </section>
     <section class="whiteboard-demo">
-        <div class="row center-block">
-            <div class="col-md-7 center-block">
+        <div class="row">
+            <div class="col-md-7" id="video-holder">
                 <div class="banner-text">Cool Explainer Videos, Whiteboard Sketch Videos, Spokesperson too!</div>
                 <div class="embed-responsive embed-responsive-16by9  banner-video">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/NiCdoZ_7kcA?rel=0&autohide=2&showinfo=0" allowfullscreen> </iframe>
                 </div>
             </div>
-            <div class="get-a-quote col-md-5 center-block">
-                <h1>Get A Quote!</h1>
-                <?php
-                $pageURL = 'http';
-                if ( array_key_exists( 'HTTPS', $_SERVER ) && $_SERVER[ "HTTPS" ] == "on" ) {
-                    $pageURL .= "s";
-                }
-                $pageURL .= "://";
-                if ( $_SERVER[ "SERVER_PORT" ] != "80" ) {
-                    $pageURL .= $_SERVER[ "SERVER_NAME" ] . ":" . $_SERVER[ "SERVER_PORT" ] . $_SERVER[ "REQUEST_URI" ];
-                } else {
-                    $pageURL .= $_SERVER[ "SERVER_NAME" ] . $_SERVER[ "REQUEST_URI" ];
-                }
-                ?>
-                <div class="formbox">
-                    <form class="contactform center-block" id="__vtigerWebForm" name="whiteboard leads" action="../forms/getInfoWhiteboard.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
-                        <fieldset>
-                            <input type="hidden" name="cf_1001" value="<?=$pageURL?>">
-                            <!-- Text input-->
-                            <div class="form-group">
-                                <div class="col-md-12">
-                                    <input name="lastname" class="form-control input-md" id="lastname" type="text" placeholder="Name">
-                                </div>
-                            </div>
-
-                            <!-- Text input-->
-                            <div class="form-group">
-                                <div class="col-md-12">
-                                    <input name="email" class="form-control input-md" id="email" type="text" placeholder="Your email">
-                                </div>
-                            </div>
-
-                            <!-- Text input-->
-                            <div class="form-group">
-                                <div class="col-md-12">
-                                    <input name="phone" class="form-control input-md" id="phone" type="text" placeholder="Contact Phone">
-                                </div>
-                            </div>
-
-                            <!-- Textarea -->
-                            <div class="form-group">
-                                <div class="col-md-12">
-                                    <textarea name="description" class="form-control" id="description" placeholder="Your Message" maxlength="900"></textarea>
-                                </div>
-                            </div>
-                            <!-- Button -->
-                            <div class="form-group">
-                                <div class="col-md-12 center-block">
-                                    <button name="submit" class="btn btn-custom" id="submit">
-                <h2 class="need-video">I Need Video!</h2>
-                </button>
-                                </div>
-                            </div>
-                        </fieldset>
-                    </form>
-                </div>
+            <div class="get-a-quote col-md-5">
+                <?php include("../forms/whiteboard.php"); ?>
             </div>
-            <div class="c"></div>
         </div>
     </section>
     <div class="container clearfix"></div>
-    <section class="container" style="margin-top:-15px">
+    <section class="container">
         <?php include("../includes/testimonial-awards.php"); ?>
     </section>
     <section class="wrap">
@@ -180,92 +125,6 @@ requirements, read the setup guide: google.com/ads/remarketingsetup -->
     <noscript>
         <div style="display:inline;"> <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1058129782/?value=0&amp;label=-jOyCO6ZkAIQ9o7H-AM&amp;guid=ON&amp;script=0"/> </div>
     </noscript>
-    <script type="text/javascript">
-        window.onload = function () {
-            var N = navigator.appName,
-                ua = navigator.userAgent,
-                tem;
-            var M = ua.match( /(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i );
-            if ( M && ( tem = ua.match( /version\/([\.\d]+)/i ) ) != null ) M[ 2 ] = tem[ 1 ];
-            M = M ? [ M[ 1 ], M[ 2 ] ] : [ N, navigator.appVersion, "-?" ];
-            var browserName = M[ 0 ];
-            var form = document.getElementById( "__vtigerWebForm" ),
-                inputs = form.elements;
-            form.onsubmit = function () {
-                var required = [],
-                    att, val;
-                for ( var i = 0; i < inputs.length; i++ ) {
-                    att = inputs[ i ].getAttribute( "required" );
-                    val = inputs[ i ].value;
-                    type = inputs[ i ].type;
-                    if ( type == "email" ) {
-                        if ( val != "" ) {
-                            var elemLabel = inputs[ i ].getAttribute( "data-label" );
-                            var emailFilter = /^[_/a-zA-Z0-9]+([!"#$%&()*+,./:;<=>?\^_`{|}~-]?[a-zA-Z0-9/_/-])*@[a-zA-Z0-9]+([\_\-\.]?[a-zA-Z0-9]+)*\.([\-\_]?[a-zA-Z0-9])+(\.?[a-zA-Z0-9]+)?$/;
-                            var illegalChars = /[\(\)\<\>\,\;\:\"\[\]]/;
-                            if ( !emailFilter.test( val ) ) {
-                                alert( "For " + elemLabel + " field please enter valid email address" );
-                                return false;
-                            } else if ( val.match( illegalChars ) ) {
-                                alert( elemLabel + " field contains illegal characters" );
-                                return false;
-                            }
-                        }
-                    }
-                    if ( att != null ) {
-                        if ( val.replace( /^\s+|\s+$/g, "" ) == "" ) {
-                            required.push( inputs[ i ].getAttribute( "data-label" ) );
-                        }
-                    }
-                }
-                if ( required.length > 0 ) {
-                    alert( "The following fields are required: " + required.join() );
-                    return false;
-                }
-                var numberTypeInputs = document.querySelectorAll( "input[type=number]" );
-                for ( var i = 0; i < numberTypeInputs.length; i++ ) {
-                    val = numberTypeInputs[ i ].value;
-                    var elemLabel = numberTypeInputs[ i ].getAttribute( "data-label" );
-                    var elemDataType = numberTypeInputs[ i ].getAttribute( "datatype" );
-                    if ( val != "" ) {
-                        if ( elemDataType == "double" ) {
-                            var numRegex = /^[+-]?\d+(\.\d+)?$/;
-                        } else {
-                            var numRegex = /^[+-]?\d+$/;
-                        }
-                        if ( !numRegex.test( val ) ) {
-                            alert( "For " + elemLabel + " field please enter valid number" );
-                            return false;
-                        }
-                    }
-                }
-                var currencyFieldInputs = document.querySelectorAll( "input[datatype='currency']" );
-                for ( var i = 0; i < currencyFieldInputs.length; i++ ) {
-                    val = currencyFieldInputs[ i ].value;
-                    var elemLabel = currencyFieldInputs[ i ].getAttribute( "data-label" );
-                    if ( val != "" ) {
-                        var numRegex = /^[+-]?\d+(,\d+)*(\.\d+)?$/;
-                        if ( !numRegex.test( val ) ) {
-                            alert( "For " + elemLabel + " field please enter valid currency value" );
-                            return false;
-                        }
-                    }
-                }
-                var dateTypeInputs = document.querySelectorAll( "input[type=date]" );
-                for ( var i = 0; i < dateTypeInputs.length; i++ ) {
-                    dateVal = dateTypeInputs[ i ].value;
-                    var elemLabel = dateTypeInputs[ i ].getAttribute( "data-label" );
-                    if ( dateVal != "" ) {
-                        var dateRegex = /^[1-9][0-9]{3}-(0[1-9]|1[0-2]|[1-9]{1})-(0[1-9]|[1-2][0-9]|3[0-1]|[1-9]{1})$/;
-                        if ( !dateRegex.test( dateVal ) ) {
-                            alert( "For " + elemLabel + " field please enter valid date in required format" );
-                            return false;
-                        }
-                    }
-                }
-            };
-        }
-    </script>
 </body>
 
 </html>
