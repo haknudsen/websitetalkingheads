129ee14ccc57893a321fc6da3f7457a92084d9f1
Andy Knudsen <andy@websitetalkingheads.com>
2017-10-06 13:43:57 -0600

    updated form on https://www.websitetalkingheads.com/explainer-video/

---
+++ explainer-video/index.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Whiteboard Video, Chalkboard Video, Animation, Explainer Video, Video Scribe, Hand Drawing Video">
+    <meta name="description" content="Talking Heads can create a Whiteboard Video, Animation, Explainer Video, Video Scribe, Hand Drawing Video, Chalkboard Video.  Here you watch examples of these kinds of videos.  Contact us for more information. Custom Whiteboard Videos, Whiteboard Animation Videos, Whiteboard Sketch Videos, Hand Drawing Videos, Chalkboard Videos, Animated Whiteboard Videos">
+    <META NAME="robots" CONTENT="index, follow">
+    <title>Talkingheads | Whiteboard Animation Videos, Sketch videos, Animated Whiteboard Videos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="fancybox/fancybox/jquery.fancybox-1.3.4.css" media="screen"/>
+    <link href="css/example-vp.css" rel="stylesheet" type="text/css"/>
+    <link href="../css/examples.css" rel="stylesheet" type="text/css"/>
+    <!-- // <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script> -->
+    <!-- // <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.1.0.js"></script>  -->
+    <link rel="stylesheet" type="text/css" href="../lightbox/js/lightbox/themes/default/jquery.lightbox.css"/>
+    <link href="css/fluid.css" rel="stylesheet" type="text/css"/>
+    <!--[if IE 6]>
+  <link rel="stylesheet" type="text/css" href="js/lightbox/themes/default/jquery.lightbox.ie6.css" />
+  <![endif]-->
+    <script type="text/javascript" src="../lightbox/js/jquery.min.js"></script>
+    <script type="text/javascript" src="../lightbox/js/lightbox/jquery.lightbox.min_v2.js"></script>
+    <!-- // <script type="text/javascript" src="src/jquery.lightbox.js"></script>   -->
+    <script type="text/javascript">
+        jQuery( document ).ready( function ( $ ) {
+            $( '.lightbox' ).lightbox();
+        } );
+    </script>
+    <?php include ('../includes/googleanalytics.php'); ?>
+</head>
+
+<body>
+    <nav role="navigation" class="navbar navbar-default">
+        <div class="nav-container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                <div class="navbar-brand">
+                    <a href="../explainer-video/">
+                        <div class="logo"></div>
+                    </a>
+                </div>
+            </div>
+            <!-- Collection of nav links and other content for toggling -->
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <div class="header-right">
+                    <div class="header-nav">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="../videopresentations/index.php">Home</a>
+                            </li>
+                            <li><a href="#">|</a>
+                            </li>
+                            <li><a href="../videopresentations/animation.php">Animation</a>
+                            </li>
+                            <li><a href="#">|</a>
+                            </li>
+                            <li><a href="../contact.php">Contact</a>
+                            </li>
+                        </ul>
+                        <div class="c"></div>
+                        <div class="header-phone">1-801.748.2281</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <section class="banner">
+        <div class="wrap">
+            <div class="row center-block">
+                <div class="col-sm-7">
+                    <div class="banner-video">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/NiCdoZ_7kcA?rel=0&autohide=2&showinfo=0" allowfullscreen></iframe>
+                        </div>
+                        <div class="banner-text">Cool Explainer Videos, Whiteboard Sketch Videos, Spokesperson too! </div>
+                    </div>
+                </div>
+
+                <div class="get-a-quote col-md-5">
+                    <?php include("../forms/whiteboard.php"); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container clearfix"></div>
+    <section class="container">
+        <?php include("../includes/testimonial-awards.php"); ?>
+    </section>
+    <div class="wrap">
+        <div class="content">
+            <div class="banner-bottom-text">Hand Drawing Videos, Whiteboard Animation Videos, Whiteboard Sketch, Video Scribe and Cartoon Doodle Videos.</div>
+            <div class="content-text">Obviously, not all whiteboard style videos are alike, so,we work with you to create the best whiteboard video for your company. We focus on supercharged messages utilizing a hand drawn whiteboard commercials for that triggers focus and engagement That's where we come in. We create high impact, exciting whiteboard illustrator VIDEOS that explain your product, service, software or business.</div>
+            <div class="examples">
+                <?php include ('../videopresentations/includes/examples-whiteboardFull.php'); ?>
+            </div>
+            <div class="c"></div>
+            <div class="call-now">
+                <h2 class="call-now-to-get-results">CALL NOW to Get Results!</h2>
+                <h2 class="call-now-to-get-results">801-748-2281</h2>
+            </div>
+            <div class="no-1">
+                <h1 class="text-center">Custom Whiteboard Video</h1>
+                <img src="../images/video-production-seal.png" alt="#1 Video Production" width="340" title="Best in Search #1 Video Production"/>
+                <h3>Keep your visitor's attention and boost sales.</h3>
+                <p>At Talking Heads we don't just make whiteboard sketch videos. We have spent years studying the art of persuasion and communication. Your story must be told well. So we collaborate to customize and create a video that boost leads and increases sales in your product or service.</p>
+                <p>Talking Heads' Sketch videos stimulate your viewers' brains. Mirror neutrons in the brain make the viewer think it's his or her hand drawing, triggering a dopamine rush in the brain and increase the viewers capacity to focus and remember. The brain is now curious and pays more attention wondering what the whiteboard drawing will become. As it tries to guess what the sketch will become, it focuses more and rewards itself. This happens over and over. Whiteboard sketch is like a time machine with each new drawing practically hypnotizing your viewer. Because of this hyper focus, the viewer also remembers more of your whiteboard animation that regular videos.</p>
+            </div>
+        </div>
+        <div class="footer"> ©
+            <?php echo date("Y")?>, <a href="https://websitetalkingheads.com" target="_blank">WebsiteTalkingHeads.com</a>. All Rights Reserved. </div>
+    </div>
+    <script type="text/javascript" src="../whiteboard/wthvideo/wthvideo.js"></script>
+    <!-- Google Code for Previous Visitors -->
+    <!-- Remarketing tags may not be associated with personally identifiable 
+information or placed on pages related to sensitive categories. For 
+instructions on adding this tag and more information on the above 
+requirements, read the setup guide: google.com/ads/remarketingsetup -->
+    <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 1058129782;
+        var google_conversion_label = "-jOyCO6ZkAIQ9o7H-AM";
+        var google_custom_params = window.google_tag_params;
+        var google_remarketing_only = true;
+        /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+        <div style="display:inline;"> <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1058129782/?value=0&amp;label=-jOyCO6ZkAIQ9o7H-AM&amp;guid=ON&amp;script=0"/> </div>
+    </noscript>
+</body>
+
+</html>
