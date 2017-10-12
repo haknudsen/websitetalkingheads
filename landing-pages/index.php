3b48e3576084dca9cc3a3df78c94f9be75bbf4d3
haknudsen <paris_giovanni@yahoo.com>
2017-10-12 17:37:58 -0600

    updated sitemap added dynamic content to landing-pages/index.php

---
+++ landing-pages/index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <title>Website Talking Heads&reg;|Landing Pages</title>
+    <meta name="keywords" content="landing pages, Audiologist,Blackboard,Blackboard Drawing,Chalkboard Video,Doodle Video,Drawing Animation,Drawing Video,Explainer-video,Explainer-whiteboard,Explanationvideo,Hand Drawing,Hand Drawing Video,Hand Drawn Video,Logo Reveals,Pros Vs Cons,Sketch Video,Typography,Video For Proposals,Video Scribe,Whiteboard">
+    <meta name="description" content="Website Talking Heads® list of our landing pages.  Explainer-video,Doodle Video,Hand Drawing,Hand Drawing Video,Drawing Video,Audiologist,Blackboard Drawing,Typography,Chalkboard Video,Drawing Animation,Explainer-whiteboard,Pros Vs Cons,Explanationvideo,Video Scribe,Blackboard,Logo Reveals,Sketch Video,Whiteboard,Hand Drawn Video">
+    <meta name="robots" content="index, follow">
+    <meta name="revisit-after" content="30 days">
+    <meta name="rating" content="general">
+    <meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8="/>
+    <meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="../css/fluid.css" rel="stylesheet" type="text/css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <?php include ('../includes/header.php'); ?>
+    <section class="jumbotron">
+       <h1>WebsiteTalkingHeads Landing Pages</h1>
+       <div class="container">
+        <dl>
+            <dt><a href="https://www.websitetalkingheads.com/audiologist" title="Audiologist">Audiologist</a></dt>
+            <dd>Page Designed for Audiologists to quickly and easily obtain a specialized whiteboard video.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/blackboard" title="Blackboard">Blackboard</a></dt>
+            <dd>In Blackboard videos rather than revealing the images on a Whiteboard, the video is made to mimic a person drawing on a Blackboard. These Videos can be found in various styles. Though the design will depend upon the artist's private style, they will try to look like something that would be drawn on a blackboard.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/blackboard_drawing" title="Blackboard Drawing">Blackboard Drawing</a></dt>
+            <dd>In Blackboard Drawing videos  the video is created to imitate somebody illustration on a Blackboard and the style of the images will be what you would see on an actual Blackboard.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/chalkboard_video" title="Chalkboard Video">Chalkboard Video</a></dt>
+            <dd>Like Blackboard Videos these are designed to look like something you could see on a Chalkboard.  We concentrate on supercharged messages making use of appealing whiteboard animations.This triggers the center and participation of the audiences' brain.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/doodle_video" title="Doodle Video">Doodle Video</a></dt>
+            <dd>These videos focus on the artit's hand as they draw the images of the video.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/drawing_animation" title="Drawing Animation">Drawing Animation</a></dt>
+            <dd>These videos show the artist's hand drawing the images of the video.  The illustrations are hand drawn</dd>
+            <dt><a href="https://www.websitetalkingheads.com/drawing_video" title="Drawing Video">Drawing Video</a></dt>
+            <dd>A Drawing video is a video that shows an artist, or more specifically just their hand, drawing images and text on a whiteboard</dd>
+            <dt><a href="https://www.websitetalkingheads.com/explainer-video" title="Explainer Video">Explainer-video</a></dt>
+            <dd>An Explainer Video is a short, fun and informative video that is usually two - three minutes long but in some cases less. An Explainer Video is a short, enjoyable. It captures and holds viewers attention more efficiently than just text or boring videos.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/explainer-whiteboard" title="Explainer-Whiteboard">Explainer-whiteboard</a></dt>
+            <dd>Whiteboard Explainer videos 'explain' what you do swiftly and effortlessly so everyone can grasp it. It's kind of like CliffsNotes for your organisation-- a concise and short version of exactly what your company does.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/explanationvideo" title="Explanation Video">Explanation Video</a></dt>
+            <dd>Explanation Videos don't simply explain ideas, they bring focus and attention to making an idea effortless to comprehend. Their communication is intentional and goal oriented. Present your company's message and sell your services and products.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/hand_drawing/" title="Hand Drawing">Hand Drawing</a></dt>
+            <dd>Hand Drawing videos are designed to tell a creative story with images created by artists.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/hand_drawing_vide" title="Hand Drawing Video">Hand Drawing Video</a></dt>
+            <dd>These designs of Whiteboard video concentrate on revealing the artist's hand illustration, sketching, or doodling the art of the video.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/hand_drawn_video" title="Hand Drawn Video">Hand Drawn Video</a></dt>
+            <dd>In hand drawn videos each new illustration practically hypnotizes your audience. Because of this active focus, the audience similarly remembers much more of your whiteboard animation that routine videos.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/logo_reveals" title="Logo Reveals">Logo Reveals</a></dt>
+            <dd>A logo reveal is a quick video clip designed to show off your logo.  It is to get a viewer's attention and expose them to your logo.  Our videos are totally shot and composted here at our office to maintain quality and exactness. We do not outsource our any part to other studios. Whatever is produced HERE.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/pros_vs_cons/" title="Pros vs Cons">Pros Vs Cons</a></dt>
+            <dd>These days, the trend for numerous sites is to include videos-- but not simply any kind of video clip. There are numerous perks to having this addition to your site, but all the very same, it is just as crucial to take into consideration the drawbacks to it.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/sketch_video" title="Sketch Video">Sketch Video</a></dt>
+            <dd>These designs of Whiteboard video concentrate on revealing the artist's hand illustration, sketching, or doodling the art of the video.  This triggers the center and participation of the audiences' brain.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/typography" title="Typography">Typography</a></dt>
+            <dd>Kinetic Typography, in some cases likewise described as movement typography, is the artistry of incorporating motion with text. Defying the conventions of horizontal text, it tries to engage an audience's attention by forcing them to aesthetically track words which cross, up or down the page. It likewise utilizes color, scale and font style choice to highlight specific words. This can in turn stimulate specific feelings in the viewer/reader, particularly when coupled with audio utilizing matching focus and modulation.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/video_for_proposals" title="Video for Proposals">Video For Proposals</a></dt>
+            <dd>Proposal Videos created by a Professional Video Production company can increase your sales and close rates. When clients are looking at the numbers, you should be reminding them WHY they should be working with you. We will work closely with you to develop a clear message that sparks their motivation to engage with your company.</dd>
+            <dt><a href="https://www.websitetalkingheads.com/video_scribe" title="Video Scribe">Video Scribe</a></dt>
+            <dd>A Video Scribe causes a dopamine rush. This increases the viewers focus, memory, and enjoyment. Your cients will watch your Scribe video. We like it! We REALLY like it!</dd>
+            <dt><a href="https://www.websitetalkingheads.com/whiteboard" title="Whiteboard">Whiteboard</a></dt>
+            <dd>A Whiteboard video is a video that shows an illustrator, or more literally just their hand, drawing images and text on a whiteboard.  Whiteboard video focus on revealing the artist's hand illustration and are short and highly focused.</dd>
+        </ul>
+       </div>
+    </section>
+
+
+    <section class="content-section-a">
+        <div class="container space">
+            <?php 
+				    $table ="whiteboard";
+				    $show ="1";
+				    include("../video_for_proposals/bestDemo.php"); 
+				?>
+        </div>
+    </section>
+
+<?php include ('../includes/footer.php'); ?>
+</body>
+</html>
