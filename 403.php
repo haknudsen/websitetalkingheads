4fd472c24d30982aa8f06b5f83026f77e78e5f3b
Andy Knudsen <andy@websitetalkingheads.com>
2017-11-08 13:21:42 -0700

    Added new whiteboard related landing pages

--- 403.php
+++ 403.php
@@ -1,56 +1,30 @@
 <!doctype html>
-<<<<<<< HEAD
-<html><!-- InstanceBegin template="/Templates/NewPhp.dwt.php" codeOutsideHTMLIsLocked="false" -->
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>Website Talking Heads - Add an Online Video Spokesperson to Your Website, Virtual Spokesperson, Website Video Spokesperson, Web Spokesperson, Website Actor</title>
-<!-- InstanceEndEditable -->
-<meta name="keywords" content="online spokesperson, video spokesperson, website talking heads, website actor, website video, transparent flash, virtual spokesperson, spokesperson, video presenter, website presenter, website spokesperson, video salesperson">
-<meta name="description" content="Online video spokesperson.  For only $199, add a virtual spokesperson to your website.  An online presenter can increase traffic conversion rates on your website.  Website video, website actor and objects to create dynamic streaming video and easily add it your existing website.">
-<meta name="robots" CONTENT="index, follow"> <!-- (Robot commands: All, None, Index, No Index, Follow, No Follow) -->
-<meta name="revisit-after" CONTENT="30 days">
-<meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8=" />
-<meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr" />
-=======
 <html>
 <head>
 <meta charset="utf-8">
-<title>Website Talking Heads - Add an Online Video Spokesperson to Your Website, Virtual Spokesperson, Website Video Spokesperson, Web Spokesperson, Website Actor</title>
-<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
-<meta name="keywords" content="online spokesperson, video spokesperson, website talking heads, website actor, website video, transparent flash, virtual spokesperson, spokesperson, video presenter, website presenter, website spokesperson, video salesperson">
-<meta name="description" content="Online video spokesperson.  For only $199, add a virtual spokesperson to your website.  An online presenter can increase traffic conversion rates on your website.  Integrate flash video, website video, website actor and objects to create dynamic streaming video and easily add it your existing website.">
-<META NAME="robots" CONTENT="index, follow"> <!-- (Robot commands: All, None, Index, No Index, Follow, No Follow) -->
-<META NAME="revisit-after" CONTENT="30 days">
-<META NAME="distribution" CONTENT="global">
-<META NAME="rating" CONTENT="general">
-<META NAME="Content-Language" CONTENT="english">
-<meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8=" />
-<meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
->>>>>>> start
+<title>Website Talking Heads - Add an Online Video Spokesperson to Your Website, Virtual Spokesperson, Website Video Spokesperson, Web Spokesperson, Website Actor</title>
+<meta name="keywords" content="online spokesperson, video spokesperson, website talking heads, website actor, website video, transparent flash, virtual spokesperson, spokesperson, video presenter, website presenter, website spokesperson, video salesperson">
+<meta name="description" content="we work with you to create the best sketch video for your company. we focus on supercharged messages utilizing a hand drawn whiteboard commercial that triggers focus and engagement">
+<meta name="robots" content="index, follow">
+<meta name="revisit-after" content="30 days">
+<meta name="distribution" content="global">
+<meta name="rating" content="general">
+<meta name="content-language" content="english">
+<meta name="verify-v1" content="ynespqoawk51pmbv7/pfklg0agx7aqpkhxxcyaxggf8=" />
+<meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-<link href="https://websitetalkingheads.com/css/style.css?v=<?php echo rand(1,100); ?>" rel="stylesheet" type="text/css" />
+<link href="css/style.css" rel="stylesheet" type="text/css">
 <link href="https://www.websitetalkingheads.com/css/fluid.css" rel="stylesheet" type="text/css" />
-<<<<<<< HEAD
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
 </head>
 <body>
-<!-- InstanceBeginEditable name="MainContent" -->
-=======
-</head>
-<body>
->>>>>>> start
 <section class="container">
     <?php include ('includes/header.php'); ?>
     <div class="errortext">
-        <p>&nbsp;</p>
-        <p>&nbsp;</p>
+        <br><br>
         <h1>403 Error</h1>
         <h2>Forbidden</h2>
         <p class="largeText2"> Sorry for the inconvenience, looks like you have run into an error. <br />
@@ -59,13 +33,7 @@
 </section>
 <?php include ('includes/footer.php'); ?>
 <?php include ('includes/chatform.php'); ?>
-<<<<<<< HEAD
-<!-- InstanceEndEditable -->  
-</body>
-<!-- InstanceEnd --></html>
-=======
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </body>
 </html>
->>>>>>> start
+</body>
+</html>
