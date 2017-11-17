970870758160ae0e5c1158e68db073c605179821
Andy Knudsen <andy@websitetalkingheads.com>
2017-11-17 13:21:05 -0700

    Working on POPUP page

---
+++ popup/base.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Talking Heads | popup test</title>
+    <META HTTP-EQUIV="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta name="keywords" content="online spokesperson, video spokesperson, website talking heads, website actor, website video, transparent flash, virtual spokesperson, spokesperson, video presenter, website presenter, website spokesperson, video salesperson">
+    <meta name="description" content="At Website Talking Heads we pride ourselves on our Professional Video Spokespeople all of which have years of experience.  No matter what term you wish to use: Video Spokesperson, Virtual Spokesperson, Website Spokesperson, Web Spokesperson, Spokes-model, Walk On Actor, Virtual Actor, Person on website, Online Spokesperson; Talking Heads has the best in the business.">
+    <meta name="robots" content="index, follow">
+    <!-- (Robot commands: All, None, Index, No Index, Follow, No Follow) -->
+    <meta name="revisit-after" content="30 days">
+    <meta name="distribution" content="global">
+    <meta name="rating" content="general">
+    <meta name="Content-Language" content="english">
+    <meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8="/>
+    <meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <link href="/css/style.css?v=<?php echo rand(1,100); ?>" rel="stylesheet" type="text/css"/>
+    <link href="/css/examples.css" rel="stylesheet" type="text/css"/>
+    <link href="../css/pricing.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/fluid.css" rel="stylesheet" type="text/css"/>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        #exitpopup {
+            text-align: center;
+        }
+        
+        #exitpopup h1 {
+            margin-top: 0px;
+            padding-top: 0px;
+        }
+        
+        #exit {
+            float: right;
+            margin: -16px -16px 0 0
+        }
+        
+        #exitpopup p {
+            text-align: left;
+        }
+    </style>
+</head>
+
+<body>
+    <div style="display: none; width:100%; height:100%; position:fixed; background:#000000; opacity: .8; filter:alpha(opacity=0.8); z-index:999998;" id="exitpopup_bg">
+
+    </div>
+        <div class="container">
+    <div style="width:600px; height:440px; margin:0px auto; display:none; position:fixed; color:#000000; padding:20px; -webkit-border-radius: 4px; -moz-border-radius: 8px; border-radius: 8px;border:thin solid #3D9CEB; z-index:999999; background:rgb(20, 20, 20); background:#FFFFFF;" id="exitpopup">
+        <h1>Wait!!! Before you go...</h1>
+        <div class="row">
+            <div class="col-md-4">
+        <div id="wthvideo"></div>
+            </div>
+            <div class="col-md-8" style="margin-top:20px">
+       <?php include ('../forms/popup-contact.php');?>
+            </div>
+        </div>
+        </div>
+    </div>
+    <?php include ('../includes/header.php'); ?>
+    <section class="page-header">
+        <h1>Website Spokesperson Pricing</h1>
+        <blockquote class="text-center">NO HIDDEN FEES, NO ANNUAL FEES, NO HOSTING FEES. This is it!</blockquote>
+        <h2>For Whitebord and Animation Videos <a href="tel://801-748-2281" title="Give us a call.">Call for Quote</a></h2>
+    </section>
+    <section class="pricing">
+        <div class="row center-block">
+            <div class="col-sm-4 center-block">
+                <div class="price-btn center-block" title="1 Thirty Second Video">
+                    <h3 class="time">1x30 Second</h3>
+                    <h4 class="price">$299</h4>
+                </div>
+                <div class="price-btn center-block" title="1 Thirty Sixty Video">
+                    <h3 class="time">1x60 Second</h3>
+                    <h4 class="price">$399</h4>
+                </div>
+            </div>
+            <div class="col-sm-4 wthvideo-prices">
+            </div>
+            <div class="col-sm-4 center-block">
+                <div class="price-btn center-block">
+                    <a href="../specials" id="featured" title="Featured Actor Scpecial">
+      <h3 class="time">Click for </h3>
+      <h3 class="time">Featured Actor</h3>
+      <h4 class="time">Special</h4>
+      </a>
+                
+
+                </div>
+                <div class="price-btn center-block" title="Multiple Video Discount">
+                    <h3 class="time">Call for</h3>
+                    <h4 class="mvd">Multiple Video</h4>
+                    <h4 class="mvd">Discounts</h4>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container-fluid my-blue">
+        <div class="col-sm-6 col-sm-offset-3">
+            <div class="row">
+                <div class="col-sm-4">
+                    <ul class="price_list">
+                        <li>30 Seconds</li>
+                        <li>Up to 90 Words</li>
+                        <li>$299</li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <ul class="price_list">
+                        <li>60 Seconds</li>
+                        <li>Up to 180 Words</li>
+                        <li>$399</li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <ul class="price_list">
+                        <li>Longer Video</li>
+                        <li>Over 180 Words</li>
+                        <li><a href="tel://801-748-2281" title="Give us a call.">Call</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    </section>
+    <section class="light-blue">
+        <h3>Featured Actor Special</h3>
+        <blockquote>Special discount packages are also available using our <a href="../specials/index.php">Featured Actor</a> for only $199! Multiple videos are done at a significantly reduced rate</blockquote>
+    </section>
+    <section class="light-grey">
+        <h3><a href="../features/index.php">Player Features</a></h3>
+        <blockquote>Your Website Spokesperson comes with the most robust, free, copyrighted player on the market today. No one has over 30 variable <a href="../features/index.php">features</a> included <span class="highlight">free.</span> You can control when the video starts, how often it plays, and much more... </blockquote>
+    </section>
+    <section class="container">
+    </section>
+    <?php include ('../includes/footer.php'); ?>
+    <script src="wthvideo/talking-popup.js"></script>
+</body>
+
+</html>
--- popup/index.php
+++ popup/index.php
@@ -4,15 +4,15 @@
 <head>
     <meta charset="utf-8">
     <title>Talking Heads | popup test</title>
-    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+    <META HTTP-EQUIV="Content-Type" content="text/html; charset=iso-8859-1">
     <meta name="keywords" content="online spokesperson, video spokesperson, website talking heads, website actor, website video, transparent flash, virtual spokesperson, spokesperson, video presenter, website presenter, website spokesperson, video salesperson">
     <meta name="description" content="At Website Talking Heads we pride ourselves on our Professional Video Spokespeople all of which have years of experience.  No matter what term you wish to use: Video Spokesperson, Virtual Spokesperson, Website Spokesperson, Web Spokesperson, Spokes-model, Walk On Actor, Virtual Actor, Person on website, Online Spokesperson; Talking Heads has the best in the business.">
-    <META NAME="robots" CONTENT="index, follow">
+    <meta name="robots" content="index, follow">
     <!-- (Robot commands: All, None, Index, No Index, Follow, No Follow) -->
-    <META NAME="revisit-after" CONTENT="30 days">
-    <META NAME="distribution" CONTENT="global">
-    <META NAME="rating" CONTENT="general">
-    <META NAME="Content-Language" CONTENT="english">
+    <meta name="revisit-after" content="30 days">
+    <meta name="distribution" content="global">
+    <meta name="rating" content="general">
+    <meta name="Content-Language" content="english">
     <meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8="/>
     <meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,28 +27,227 @@
     <link href="/css/fluid.css" rel="stylesheet" type="text/css"/>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <style type="text/css">
-        #exitpopup {
-            text-align: center;
-        }
-        
-        #exitpopup h1 {
-            margin-top: 0px;
-            padding-top: 0px;
-        }
-        
-        #exit {
-            float: right;
-            margin: -16px -16px 0 0
-        }
-        
-        #exitpopup p {
-            text-align: left;
-        }
-    </style>
 </head>
 
 <body>
+    <div id="exitpopup_bg">
+
+    </div>
+    <div class="container">
+        <div id="exitpopup">
+            <h1 id="popup_title">Wait!!! Before you go...</h1>
+            <div class="row">
+                <div class="col-md-5">
+                    <div id="wthvideo"></div>
+                </div>
+                <div class="col-md-7" style="padding: 0 1rem">
+                    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+                    <form name="PopUp Form" id="__vtigerWebForm" action="https://websitetalkingheads.od1.vtiger.com/modules/Webforms/capture.php" enctype="multipart/form-data" method="post" accept-charset="utf-8"><input name="__vtrftk" type="hidden" value="sid:57cb0108784ff149dc1ce64742e721c0f1ab648f,1510872241">
+                        <input name="publicid" type="hidden" value="5979b5c0472a3584b88b3220f2367c07">
+                        <input name="urlencodeenable" type="hidden" value="1">
+                        <input name="name" type="hidden" value="PopUp Form">
+                        <input name="__vtCurrency" type="hidden" value="1">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><label>Name*</label>
+                                    </td>
+                                    <td>
+                                        <input name="lastname" required="" type="text" value="" data-label="name"> </td>
+                                </tr>
+                                <tr>
+                                    <td><label>Email*</label>
+                                    </td>
+                                    <td>
+                                        <input name="email" required="" type="email" value="" data-label="email"> </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <script src="https://www.google.com/recaptcha/api.js"></script>
+                        <div class="g-recaptcha" data-sitekey="6LcmdSATAAAAAGWw734vGo0AXQwuxJS7RmDZA_Fe"></div>
+                        <input id="captchaUrl" type="hidden" value="https://websitetalkingheads.od1.vtiger.com/modules/Settings/Webforms/actions/ValidateCaptcha.php">
+                        <input name="recaptcha_validation_value" id="recaptcha_validation_value" type="hidden">
+                        <input class="btn-popup" id="vtigerFormSubmitBtn" type="submit" value="SEND">
+                    </form>
+                    <script type="text/javascript">
+                        window.onload = function () {
+                            var N = navigator.appName,
+                                ua = navigator.userAgent,
+                                tem;
+                            var M = ua.match( /(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i );
+                            if ( M && ( tem = ua.match( /version\/([\.\d]+)/i ) ) != null ) M[ 2 ] = tem[ 1 ];
+                            M = M ? [ M[ 1 ], M[ 2 ] ] : [ N, navigator.appVersion, "-?" ];
+                            var browserName = M[ 0 ];
+                            var form = document.getElementById( "__vtigerWebForm" ),
+                                inputs = form.elements;
+                            form.onsubmit = function () {
+                                document.getElementById( "vtigerFormSubmitBtn" ).disabled = true;
+                                var required = [],
+                                    att, val;
+                                var startDate;
+                                var endDate;
+                                for ( var i = 0; i < inputs.length; i++ ) {
+                                    att = inputs[ i ].getAttribute( "required" );
+                                    val = inputs[ i ].value;
+                                    type = inputs[ i ].type;
+                                    if ( inputs[ i ].name == "birthday" ) {
+                                        birthdayDate = new Date( inputs[ i ].value );
+                                        todayDate = new Date();
+                                        todayDate.setHours( 0, 0, 0, 0 );
+                                        birthdayDate.setHours( 0, 0, 0, 0 );
+                                        if ( birthdayDate >= todayDate ) {
+                                            alert( "Date of Birth should be less than Today's Date." );
+                                            document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                            return false;
+                                        }
+                                    }
+                                    if ( inputs[ i ].name == "support_start_date" ) {
+                                        startDate = inputs[ i ].value;
+                                    }
+                                    if ( inputs[ i ].name == "support_end_date" ) {
+                                        endDate = inputs[ i ].value;
+                                    }
+                                    if ( type == "email" ) {
+                                        if ( val != "" ) {
+                                            var elemLabel = inputs[ i ].getAttribute( "label" );
+                                            var emailFilter = /^[_/a-zA-Z0-9]+([!"#$%&()*+,./:;<=>?\^_`{|}~-]?[a-zA-Z0-9/_/-])*@[a-zA-Z0-9]+([\_\-\.]?[a-zA-Z0-9]+)*\.([\-\_]?[a-zA-Z0-9])+(\.?[a-zA-Z0-9]+)?$/;
+                                            var illegalChars = /[\(\)\<\>\,\;\:\"\[\]]/;
+                                            if ( !emailFilter.test( val ) ) {
+                                                alert( "For " + elemLabel + " field please enter valid email address" );
+                                                document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                                return false;
+                                            } else if ( val.match( illegalChars ) ) {
+                                                alert( elemLabel + " field contains illegal characters" );
+                                                document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                                return false;
+                                            }
+                                        }
+                                    }
+                                    if ( startDate && typeof startDate !== "undefined" ) {
+                                        if ( endDate && typeof endDate !== "undefined" ) {
+                                            startDate = new Date( startDate );
+                                            endDate = new Date( endDate );
+                                            if ( endDate < startDate ) {
+                                                alert( "Support End Date should be greater than or equal to Support Start Date" );
+                                                document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                                return false;
+                                            }
+                                        }
+                                    }
+                                    if ( att != null ) {
+                                        if ( val.replace( /^\s+|\s+$/g, "" ) == "" ) {
+                                            required.push( inputs[ i ].getAttribute( "label" ) );
+                                        }
+                                    }
+                                }
+                                if ( required.length > 0 ) {
+                                    alert( "The following fields are required: " + required.join() );
+                                    document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                    return false;
+                                }
+                                var numberTypeInputs = document.querySelectorAll( "input[type=number]" );
+                                for ( var i = 0; i < numberTypeInputs.length; i++ ) {
+                                    val = numberTypeInputs[ i ].value;
+                                    var elemLabel = numberTypeInputs[ i ].getAttribute( "label" );
+                                    var elemDataType = numberTypeInputs[ i ].getAttribute( "datatype" );
+                                    if ( val != "" ) {
+                                        if ( elemDataType == "double" ) {
+                                            var numRegex = /^[+-]?\d+(\.\d+)?$/;
+                                        } else {
+                                            var numRegex = /^[+-]?\d+$/;
+                                        }
+                                        if ( !numRegex.test( val ) ) {
+                                            alert( "For " + elemLabel + " field please enter valid number" );
+                                            document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                            return false;
+                                        }
+                                    }
+                                }
+                                var dateTypeInputs = document.querySelectorAll( "input[type=date]" );
+                                for ( var i = 0; i < dateTypeInputs.length; i++ ) {
+                                    dateVal = dateTypeInputs[ i ].value;
+                                    var elemLabel = dateTypeInputs[ i ].getAttribute( "label" );
+                                    if ( dateVal != "" ) {
+                                        var dateRegex = /^[1-9][0-9]{3}-(0[1-9]|1[0-2]|[1-9]{1})-(0[1-9]|[1-2][0-9]|3[0-1]|[1-9]{1})$/;
+                                        if ( !dateRegex.test( dateVal ) ) {
+                                            alert( "For " + elemLabel + " field please enter valid date in required format" );
+                                            document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                            return false;
+                                        }
+                                    }
+                                }
+                                var inputElems = document.getElementsByTagName( "input" );
+                                var totalFileSize = 0;
+                                for ( var i = 0; i < inputElems.length; i++ ) {
+                                    if ( inputElems[ i ].type.toLowerCase() === "file" ) {
+                                        var file = inputElems[ i ].files[ 0 ];
+                                        if ( typeof file !== "undefined" ) {
+                                            var totalFileSize = totalFileSize + file.size;
+                                        }
+                                    }
+                                }
+                                if ( totalFileSize > 52428800 ) {
+                                    alert( "Maximum allowed file size including all files is 50MB." );
+                                    document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                    return false;
+                                }
+                                var inputElem = document.querySelectorAll( "input[type=file]" );
+                                var fileSize = 0;
+                                for ( var i = 0; i < inputElem.length; i++ ) {
+                                    if ( inputElem[ i ].type.toLowerCase() === "file" ) {
+                                        if ( inputElem[ i ].hasAttribute( "selectedTypeImage" ) ) {
+                                            var imageFile = inputElem[ i ].files[ 0 ];
+                                            var fileSize = imageFile.size;
+                                        }
+                                    }
+                                    if ( fileSize > 5242880 ) {
+                                        alert( "Maximum allowed image size is 5MB." );
+                                        document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                        return false;
+                                    }
+                                }
+                                document.getElementById( "vtigerFormSubmitBtn" ).disabled = true;
+                                var recaptchaValidationValue = document.getElementById( "recaptcha_validation_value" ).value;
+                                if ( recaptchaValidationValue != true ) {
+                                    var recaptchaResponse = document.getElementsByName( "g-recaptcha-response" )[ 0 ].value;
+                                    var validationUrl = document.getElementById( "captchaUrl" ).value + "?recaptcha_response=" + recaptchaResponse + "&current_url=" + window.location.href + "&callback=captchaCallback";
+                                    jsonp.fetch( validationUrl );
+                                    return false;
+                                }
+                            };
+                        };
+                        var jsonp = {
+                            callbackCounter: 0,
+                            fetch: function ( url ) {
+                                url = url + "&callId=" + this.callbackCounter;
+                                var scriptTag = document.createElement( "SCRIPT" );
+                                scriptTag.src = url;
+                                scriptTag.async = true;
+                                scriptTag.id = "captchaCallback_" + this.callbackCounter;
+                                scriptTag.type = "text/javascript";
+                                document.getElementsByTagName( "HEAD" )[ 0 ].appendChild( scriptTag );
+                                this.callbackCounter++;
+                            }
+                        };
+
+                        function captchaCallback( data ) {
+                            if ( data.result.success == true ) {
+                                document.getElementById( "recaptcha_validation_value" ).value = true;
+                                var form = document.getElementById( "__vtigerWebForm" );
+                                form.submit();
+                            } else {
+                                document.getElementById( "vtigerFormSubmitBtn" ).disabled = false;
+                                grecaptcha.reset();
+                                alert( "Captcha not verified. Please verify captcha." );
+                            }
+                            var element = document.getElementById( "captchaCallback_" + data.result.callId );
+                            element.parentNode.removeChild( element );
+                        }
+                    </script>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include ('../includes/header.php'); ?>
     <section class="page-header">
         <h1>Website Spokesperson Pricing</h1>
@@ -77,6 +276,8 @@
       <h4 class="time">Special</h4>
       </a>
                 
+
+
                 </div>
                 <div class="price-btn center-block" title="Multiple Video Discount">
                     <h3 class="time">Call for</h3>
@@ -117,11 +318,14 @@
         <h3>Featured Actor Special</h3>
         <blockquote>Special discount packages are also available using our <a href="../specials/index.php">Featured Actor</a> for only $199! Multiple videos are done at a significantly reduced rate</blockquote>
     </section>
-    
+    <section class="light-grey">
+        <h3><a href="../features/index.php">Player Features</a></h3>
+        <blockquote>Your Website Spokesperson comes with the most robust, free, copyrighted player on the market today. No one has over 30 variable <a href="../features/index.php">features</a> included <span class="highlight">free.</span> You can control when the video starts, how often it plays, and much more... </blockquote>
+    </section>
     <section class="container">
     </section>
     <?php include ('../includes/footer.php'); ?>
-    <script src="wthvideo/talking-heads-popup.js"></script>
+    <script src="wthvideo/talking-popup.js"></script>
 </body>
 
 </html>
